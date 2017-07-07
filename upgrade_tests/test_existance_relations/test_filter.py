31fe57735f0abb60ae0c4627795b829373c6eb4c
renzon <renzon@gmail.com>
2017-07-07 13:23:08 -0300

    Refactored all filter test cases to only one

--- upgrade_tests/test_existance_relations/test_filter.py
+++ upgrade_tests/test_existance_relations/test_filter.py
@@ -23,84 +23,21 @@ from upgrade_tests.helpers.existence import compare_postupgrade, pytest_ids, \
     check_missing_entity
 
 # Required Data
-component = 'filter'
-fil_rtype = compare_postupgrade(component, 'resource type')
-fil_search = compare_postupgrade(component, 'search')
-fil_unlimited = compare_postupgrade(component, 'unlimited?')
-fil_role = compare_postupgrade(component, 'role')
-fil_perm = compare_postupgrade(component, 'permissions')
-
-
-@pytest.mark.parametrize("pre,post", fil_rtype, ids=pytest_ids(fil_rtype))
-def test_positive_filters_by_resource_type(pre, post):
-    """Test all filters of all roles are existing after upgrade by resource
-    types
-
-    :id: upgrade-362f4b0c-49bb-424d-92e4-446ec59b8c5c
-
-    :expectedresults: All filters of all roles should be retained post upgrade
-        by resource types
-    """
-    msg = check_missing_entity(pre, post)
-    if msg:
-        pytest.fail(msg)
-    assert pre == post
-
-
-@pytest.mark.parametrize("pre,post", fil_search, ids=pytest_ids(fil_search))
-def test_positive_filters_by_search(pre, post):
-    """Test all filters search criteria is existing after upgrade
-
-    :id: upgrade-da2dd076-f0e6-45ee-8a5d-99f2e083aabc
-
-    :expectedresults: All filters search criteria should be retained post
-        upgrade
-    """
-    msg = check_missing_entity(pre, post)
-    if msg:
-        pytest.fail(msg)
-    assert pre == post
+filter_attrs = []
+for attr in ['resource type', 'search', 'unlimited?', 'role', 'permissions']:
+    filter_attrs.extend(compare_postupgrade('filter', attr))
 
 
 @pytest.mark.parametrize(
-    "pre,post", fil_unlimited, ids=pytest_ids(fil_unlimited))
-def test_positive_filters_by_unlimited_check(pre, post):
-    """Test all filters unlimited criteria is existing after upgrade
+    "pre,post", filter_attrs, ids=pytest_ids(filter_attrs))
+def test_positive_all_filter_attributes(pre, post):
+    """Test all filters of all possible filter attributes are present after
+    upgrade
 
-    :id: upgrade-abf65640-dc9b-415e-846d-0df43391228f
+    :id: upgrade-362f4b0c-49bb-424d-92e4-446ec59b8c5c
 
-    :expectedresults: All filters unlimited criteria should be retained post
-        upgrade
-    """
-    msg = check_missing_entity(pre, post)
-    if msg:
-        pytest.fail(msg)
-    assert pre == post
-
-
-@pytest.mark.parametrize("pre,post", fil_role, ids=pytest_ids(fil_role))
-def test_positive_filters_by_role(pre, post):
-    """Test all filters association with role is existing after upgrade
-
-    :id: upgrade-dffdc0ac-a4b5-4b70-8e4d-fb37765f75ed
-
-    :expectedresults: All filters association with role should be retained post
-        upgrade
-    """
-    msg = check_missing_entity(pre, post)
-    if msg:
-        pytest.fail(msg)
-    assert pre == post
-
-
-@pytest.mark.parametrize("pre,post", fil_perm, ids=pytest_ids(fil_perm))
-def test_positive_filters_by_permissions(pre, post):
-    """Test all filters all permissions are existing after upgrade
-
-    :id: upgrade-dd4fab7e-bd8f-4645-8aab-42a14ffe3a0e
-
-    :expectedresults: All filters all permissions should be retained post
-        upgrade
+    :expectedresults: All filters of all attributes should be retained post
+    upgrade
     """
     msg = check_missing_entity(pre, post)
     if msg:
